29f51fe1c30bf090da2dcd5fb79b2a87bf8c01f0
ElijahBundi <bundielly@gmail.com>
2023-01-30 15:09:57 +0300

    Hero complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div>
-      <header>
-        Welcome to TrackEA
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
